f00fc58191504e0f72137893d348af86623cb065
javier <vonfreiren@protonmail.com>
2023-04-28 15:09:58 +0200

    Configuration example

--- main.py
+++
@@ -1,4 +0,0 @@
-from spotify import search_spotify
-
-name = 'Martin Garrix'
-
--- spotify.py
+++ spotify.py
@@ -2,7 +2,8 @@ import csv
 
 import spotipy
 import yaml
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import os
 
 from youtube import search_youtube
 from youtube_playlist import insert_playlist
@@ -12,14 +13,24 @@ with open('config.yaml') as f:
 
 client_id = data['spotify_client_id']
 secret = data['spotify_secret']
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
-                                                           client_secret=secret))
+user_id = data['spotify_user_id']
 
 
-def search_spotify(name_artist):
+os.environ["SPOTIPY_REDIRECT_URI"] = "https://example.com/callback"
+
+scope = "user-library-read user-library-modify playlist-modify-public"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=secret,
+                                               scope=scope))
+
+def search_spotify_artist(name_artist, song=None, include_youtube=False):
+    lists_uris_spotify = []
     list_video_ids = []
-    results = sp.search(q=name_artist, limit=1)
+    if song is not None:
+        results = sp.search(q=name_artist + song, limit=1)
+    else:
+        results = sp.search(q=name_artist, limit=1)
     artist_id = results['tracks']['items'][0]['artists'][0]['id']
     related_artists = sp.artist_related_artists(artist_id)['artists']
     with open('songs.csv', 'w', newline='') as file:
@@ -30,12 +41,37 @@ def search_spotify(name_artist):
         for artist in related_artists:
             top_tracks = sp.artist_top_tracks(artist['id'])
             for idx, track in enumerate(top_tracks['tracks']):
-                yt_url, yt_id = search_youtube(track['name']+' '+artist['name'])
+                lists_uris_spotify.append(track['uri'])
+                if include_youtube:
+                    yt_url, yt_id = search_youtube(track['name']+' '+artist['name'])
                     # Write each line to the file
-                writer.writerow([artist['name'], track['name'], yt_url])
-                list_video_ids.append(yt_id)
-    insert_playlist(name_artist, list_video_ids)
+                    writer.writerow([artist['name'], track['name'], yt_url])
+                    list_video_ids.append(yt_id)
+    add_playlist_spotify(name_artist, lists_uris_spotify)
+    #insert_playlist(name_artist, list_video_ids)
 
 
 
+def add_playlist_spotify(name_artist,lists_uris_spotify ):
+    playlist_name = name_artist +' Similar Songs'
 
+    new_playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=True)
+
+    # Replace the list of track URIs with the URIs of the tracks you want to add
+    sp.playlist_add_items(playlist_id=new_playlist['id'], items=lists_uris_spotify[:99])
+
+def add_playlist(artist_name):
+    # Replace the playlist name and description with your own values
+    playlist_name = 'Similar artists to ' + artist_name + ' on Spotify'
+    playlist_description = 'Tracks from Similar artists to ' + artist_name + ' on Spotify'
+
+    # Create the new playlist
+    playlist = sp.user_playlist_create(user='javfreire@protonmail.com', name=playlist_name, public=True, description=playlist_description)
+
+    # Print the playlist ID
+    print(f'Playlist created! ID: {playlist["id"]}')
+
+
+artist = 'Kygo'
+song = 'Firestone'
+search_spotify_artist('Kygo', song=song)
